6752a882afc184a8eb29ecb40d626c161b1484fa
Henrik Moe Wæhre <henrikmw@live.no>
2024-02-01 15:53:24 +0100

    🔐Add dotenv

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -26,9 +26,8 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("io.ktor:ktor-client-apache:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation ("io.github.jan-tennert.supabase:postgrest-kt:$supabase_version")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- server/src/main/kotlin/dagensord/plugins/Routing.kt
+++ server/src/main/kotlin/dagensord/plugins/Routing.kt
@@ -1,17 +1,15 @@
 package dagensord.plugins
 
-import dagensord.db.supabase
-import dagensord.model.Expression
-import io.github.jan.supabase.postgrest.from
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
+import io.github.cdimascio.dotenv.dotenv
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            val expression = supabase.from("expressions").select().decodeSingle<Expression>()
-            println(expression)
-
+            println("Hello world")
+            val dotenv = dotenv()
+            println(dotenv["URL"])
         }
     }
 }
