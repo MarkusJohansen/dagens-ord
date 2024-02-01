de04ddaf4230f22047566857c1e25c563c7491bf
Henrik Moe Wæhre <42566233+henrikmw@users.noreply.github.com>
2024-02-01 16:00:11 +0100

    ➕Add dotenv (#17)
    * 🔐Add dotenv
    
    * 🙈Add .env to gitignore

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,10 +1,12 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val supabase_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.22"
     id("io.ktor.plugin") version "2.3.7"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
 
 group = "dagensord"
@@ -12,9 +14,9 @@ version = "0.0.1"
 
 application {
     mainClass.set("dagensord.ApplicationKt")
-
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+
 }
 
 repositories {
@@ -25,6 +27,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- server/src/main/kotlin/dagensord/plugins/Routing.kt
+++ server/src/main/kotlin/dagensord/plugins/Routing.kt
@@ -1,13 +1,15 @@
 package dagensord.plugins
 
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.github.cdimascio.dotenv.dotenv
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            println("Hello world")
+            val dotenv = dotenv()
+            println(dotenv["URL"])
         }
     }
 }
