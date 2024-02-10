7ae07c60970dd022ea6758f5e14d441cc208f046
Henrik Moe Wæhre <42566233+henrikmw@users.noreply.github.com>
2024-02-10 14:43:05 +0100

    6 connect ktor server to supabase (#18)

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -6,7 +6,7 @@ val supabase_version: String by project
 plugins {
     kotlin("jvm") version "1.9.22"
     id("io.ktor.plugin") version "2.3.7"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+    kotlin("plugin.serialization") version "1.9.22"
 }
 
 group = "dagensord"
@@ -26,7 +26,9 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-client-apache:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation ("io.github.jan-tennert.supabase:postgrest-kt:$supabase_version")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- server/src/main/kotlin/dagensord/plugins/Routing.kt
+++ server/src/main/kotlin/dagensord/plugins/Routing.kt
@@ -1,15 +1,28 @@
 package dagensord.plugins
 
+import dagensord.db.supabase
+import dagensord.model.Expression
+import io.github.jan.supabase.postgrest.from
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.github.cdimascio.dotenv.dotenv
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            println("Hello world")
-            val dotenv = dotenv()
-            println(dotenv["URL"])
+            call.respond("Hello world")
         }
+
+        get("/expressions") {
+            try {
+                val expressions = supabase.from("expressions").select().decodeList<Expression>()
+                call.respond(HttpStatusCode.OK, expressions.toString())
+                println(expressions.toString())
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.OK, e.message.toString())
+            }
+        }
+
     }
 }
