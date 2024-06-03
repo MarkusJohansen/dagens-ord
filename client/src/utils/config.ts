3c7c09f88be1298436edd09ff7342ee0b9170fa7
Markus A. R. Johansen <90006516+MarkusJohansen@users.noreply.github.com>
2024-06-03 23:48:03 +0200

    feat: set environment variable in workflow
    We cant rely on a local .env for the variables to be set in vercel dynamically.
    use .env locally, sets variable when in prod from github secrets.

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,10 +1,3 @@
-const notNull = <T>(value: T | undefined | null, name: string): T => {
-  if (value == null) {
-    throw new TypeError(`${name} cannot be nullable! Value: ${value}`);
-  }
-  return value;
-};
-
 export const config = {
-  API_URL: notNull(import.meta.env.VITE_API_URL, "meta.env.VITE_API_URL"),
-} as const;
+  API_URL: import.meta.env.VITE_SERVER_URL,
+};
