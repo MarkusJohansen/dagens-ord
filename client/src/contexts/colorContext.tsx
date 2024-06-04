eaa30e73bffcb9c8ef3c87ec767228e076fa4f4a
Markus A. R. Johansen <90006516+MarkusJohansen@users.noreply.github.com>
2024-06-05 00:21:22 +0200

    fix: color refresh once every 24hr (#56)
    * fix: color refresh once every 24hr
    
    * feat: Remove console.log statement and improve color refresh logic
    
    * 24hr refresh time

--- client/src/contexts/colorContext.tsx
+++ client/src/contexts/colorContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 type ColorContextType = {
   color: string;
@@ -21,7 +21,30 @@ export const ColorProvider: React.FC<{ children: React.ReactNode }> = ({
     "#faedcd", //tan
   ];
 
-  const [color] = useState(colors[Math.floor(Math.random() * colors.length)]);
+  const [color, setColor] = useState(() => {
+    const cachedColor = localStorage.getItem("color");
+    return cachedColor
+      ? cachedColor
+      : colors[Math.floor(Math.random() * colors.length)];
+  });
+
+  const updateColor = () => {
+    const newColor = colors[Math.floor(Math.random() * colors.length)];
+    setColor(newColor);
+    localStorage.setItem("color", newColor);
+    localStorage.setItem("colorUpdateTime", Date.now().toString());
+    return newColor;
+  };
+
+  useEffect(() => {
+    const colorUpdateTime = localStorage.getItem("colorUpdateTime");
+    if (
+      !colorUpdateTime ||
+      Date.now() - Number(colorUpdateTime) > 24 * 60 * 60 * 1000
+    ) {
+      updateColor();
+    }
+  }, []);
 
   return (
     <ColorContext.Provider value={{ color }}>{children}</ColorContext.Provider>
