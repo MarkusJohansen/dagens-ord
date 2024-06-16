6ac226500649afdf6a99a03d1b50d724d63475b6
Markus A. R. Johansen <90006516+MarkusJohansen@users.noreply.github.com>
2024-06-16 23:33:45 +0200

    delete tailwind animation config and sidebar context

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,20 +8,6 @@ export default {
         "dark-gray": "#363636",
         "semi-transparent": "rgba(0, 0, 0, 0.2)",
       },
-      keyframes: {
-        "slide-in": {
-          "0%": { transform: "translateX(-100%)" },
-          "100%": { transform: "translateX(0)" },
-        },
-        "slide-out": {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
-      },
-      animation: {
-        "slide-in": "slide-in 0.5s ease-in-out forwards",
-        "slide-out": "slide-out 0.5s ease-in-out forwards",
-      },
     },
   },
   plugins: [],
