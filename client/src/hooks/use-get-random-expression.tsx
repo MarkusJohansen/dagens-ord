bb3505b4f563e4c51bfc6b5b99f56551524d8fe0
Henrik Moe Wæhre <henrikmw@live.no>
2024-07-18 18:35:47 +0200

    Only change expression once a day

--- client/src/hooks/use-get-random-expression.tsx
+++ client/src/hooks/use-get-random-expression.tsx
@@ -8,15 +8,31 @@ export const useGetNewExpression = () => {
 
   useEffect(() => {
     const fetchNewExpression = async () => {
-      if (!expression) {
+      const lastFetchDate = localStorage.getItem("lastFetchDate");
+      const today = new Date().toISOString().split("T")[0];
+      const storedExpression = localStorage.getItem("storedExpression");
+
+      if (storedExpression && lastFetchDate === today) {
+        // Use the stored expression if it's still valid for today
+        setExpression(JSON.parse(storedExpression));
+        setIsLoading(false);
+      } else {
+        // Fetch a new expression and update storage
+        setIsLoading(true);
         const { data, error } = await fetchAllExpressions();
         if (error) {
           console.error("Error fetching new expression", error);
+          setIsLoading(false);
         }
         if (data) {
           const randomIndex = Math.floor(Math.random() * data.length);
-          const randomRow = data[randomIndex];
-          setExpression(randomRow);
+          const randomExpression = data[randomIndex];
+          setExpression(randomExpression);
+          localStorage.setItem("lastFetchDate", today);
+          localStorage.setItem(
+            "storedExpression",
+            JSON.stringify(randomExpression)
+          );
           setIsLoading(false);
         }
       }
