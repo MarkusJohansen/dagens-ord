7800a886294c7a1f19f95d999f972a847a85b403
henrikmw <henrikmw@live.no>
2024-10-02 13:38:33 +0200

    Change vite config to esm format

--- admin-app/vite.config.ts
+++ admin-app/vite.config.ts
@@ -1,11 +1,18 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	css: {
+  plugins: [
+    sveltekit()
+  ],
+  css: {
     postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
-    },
-  },
+      plugins: [
+        tailwindcss(),
+        autoprefixer()
+      ]
+    }
+  }
 });
