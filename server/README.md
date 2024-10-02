cd2c610119f39b1102bad05dabcd7766a1f6f93d
Markus A. R. Johansen <90006516+MarkusJohansen@users.noreply.github.com>
2024-10-02 20:39:45 +0200

    feat: fastAPI  server

---
+++ server/README.md
@@ -0,0 +1,3 @@
+# Server app
+
+will in due time handle all the database interaction
---
+++ server/server/main.py
@@ -0,0 +1,64 @@
+import os
+from fastapi import FastAPI
+from .expression import Expression
+from supabase import create_client, Client
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Create a new Supabase client and fastapi app
+supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+app = FastAPI()
+
+#GETS
+@app.get("/")
+async def get_all_expressions():
+    return supabase.table("expressions").select("*").execute()
+
+
+@app.get("/expression_count")
+async def fetch_expression_count():
+    return {"data": supabase.table("expressions").select("id", count="exact").execute().count}
+
+@app.post("/search/{query}")
+async def search_expression(query: str):
+    return supabase.table("expressions").select("*").ilike("expression", f"%{query}%").execute()
+
+
+
+#TODO: Implement the following routes
+# @app.get("/random_expression")
+# async def fetch_random_expression():
+#     return supabase.table("expressions").select("*").eq("nsfw", False).limit(1).execute()
+    
+# @app.get("/suggestion_count")
+# async def fetch_suggestion_count():
+#     return {"count": supabase.table("suggestions").select("id", count="exact").execute().count}
+
+# @app.get("/suggestions") 
+# async def fetch_all_suggestions():
+#     return {"data": supabase.table("suggestions").select("*").execute()}
+
+# #POSTS    
+# @app.post("/suggest/{expression}")
+# async def suggest_expression(expression: Expression):
+#     return {"data": supabase.table("suggestions").insert({"expression": expression}).execute()}
+
+# @app.post("/add/{suggest_id}")
+# async def add_expression(suggest_id: int):
+#     suggestion = supabase.table("suggestions").select("expression").eq("id", suggest_id).execute()
+#     expression = Expression(
+#         expression=suggestion["expression"],
+#         example=suggestion["example"],
+#         definition=suggestion["definition"]
+#     )
+#     return {"data": supabase.table("expressions").insert({"expression": expression}).execute()}
+
+# # DELETES
+# @app.delete("/delete/{suggest_id}")
+# async def delete_suggestion(suggest_id: int):
+#     return {"data": supabase.table("suggestions").eq("id", suggest_id).delete().execute()}
+
+# #PUTS
