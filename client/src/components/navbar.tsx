c94333c2c0b6b3e22917b3d0fcaa520aa982122b
henrikmw <henrikmw@live.no>
2024-10-04 10:44:10 +0200

    First version of sidebar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -6,8 +6,8 @@ import { useSearchContext } from "@/context/search-context";
 import SearchBar from "./search-bar";
 import { Navlink } from "./navlink";
 import Streak from "./streak";
-import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { FaBars } from "react-icons/fa";
+import Sidebar from "./sidebar";
 
 const Navbar = () => {
   const { color } = useContext(ColorContext);
@@ -20,15 +20,16 @@ const Navbar = () => {
   const handleSearch = () => {
     search(query);
     navigate({ to: "/sok" });
+    setSidebarOpen(false);
   };
 
   // Check if the screen size is mobile
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 768); // Adjust the breakpoint as needed
+      setIsMobile(window.innerWidth < 768);
     };
 
-    handleResize(); // Initialize the state
+    handleResize();
     window.addEventListener("resize", handleResize);
 
     return () => window.removeEventListener("resize", handleResize);
@@ -36,6 +37,10 @@ const Navbar = () => {
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
   };
 
   return (
@@ -58,7 +63,6 @@ const Navbar = () => {
               color={color}
             />
           </div>
-          {/* Hamburger Button for Mobile */}
           {isMobile && (
             <div className="flex">
               <Streak />
@@ -67,45 +71,28 @@ const Navbar = () => {
               </button>
             </div>
           )}
-
         </div>
         <hr className="w-full m-0" />
       </nav>
 
-      {/* Sidebar for Mobile */}
       {isMobile && (
-        <Sidebar
-          style={{
-            position: "fixed",
-            backgroundColor: color,
-          }}
-          className={`h-screen top-0 right-0 transition-transform duration-300 ease-in-out ${sidebarOpen ? "translate-x-0" : "translate-x-full"}`}
-        >
-          <Menu>
-            <MenuItem>
-            {/* Fikse så den lukker seg når du søker */}
-              <SearchBar
-                query={query}
-                setQuery={setQuery}
-                handleSearch={handleSearch}
-                color={color}
-              />
-            </MenuItem>
-            <MenuItem onClick={toggleSidebar}>
-              <Navlink to="/om-prosjektet" label="Om prosjektet" />
-            </MenuItem>
-            <MenuItem onClick={toggleSidebar}>
-              <Navlink to="/bidra" label="Bidra" />
-            </MenuItem>
-            <MenuItem onClick={toggleSidebar}>
-              <button onClick={toggleSidebar} className="text-sm">
-                Lukk
-              </button>
-            </MenuItem>
-          </Menu>
-          <div>
-          </div>
-        </Sidebar>
+        <>
+          <Sidebar
+            sidebarOpen={sidebarOpen}
+            toggleSidebar={toggleSidebar}
+            color={color}
+            query={query}
+            setQuery={setQuery}
+            handleSearch={handleSearch}
+          />
+
+          <div
+            className={`fixed inset-0 bg-black z-40 transition-opacity duration-300 ${
+              sidebarOpen ? "opacity-50" : "opacity-0 pointer-events-none"
+            }`}
+            onClick={closeSidebar}
+          />
+        </>
       )}
     </>
   );
---
+++ client/src/components/sidebar.tsx
@@ -0,0 +1,58 @@
+import { Dispatch, SetStateAction } from "react";
+import { Navlink } from "./navlink";
+import SearchBar from "./search-bar";
+
+interface SidebarProps {
+  sidebarOpen: boolean;
+  toggleSidebar: () => void;
+  color: string;
+  query: string;
+  setQuery: Dispatch<SetStateAction<string>>;
+  handleSearch: () => void;
+}
+
+const Sidebar = ({
+  sidebarOpen,
+  toggleSidebar,
+  color,
+  query,
+  setQuery,
+  handleSearch,
+}: SidebarProps) => {
+  return (
+    <div
+      className={`fixed top-0 right-0 h-screen bg-white z-50 transition-transform duration-300 ease-in-out ${
+        sidebarOpen ? "translate-x-0" : "translate-x-full"
+      }`}
+      style={{
+        backgroundColor: color, // Use dynamic color from context
+      }}
+    >
+      <div className="flex flex-col h-full w-64 p-4">
+        <div className="flex justify-end">
+          <button onClick={toggleSidebar} className="bg-transparent text-black active:bg-black active:bg-opacity-30 active:border-none">
+            X
+          </button>
+        </div>
+        <div className="flex-1">
+          <div className="mb-4">
+            <SearchBar
+              query={query}
+              setQuery={setQuery}
+              handleSearch={handleSearch}
+              color={color}
+            />
+          </div>
+          <div className="mb-4" onClick={toggleSidebar}>
+            <Navlink to="/om-prosjektet" label="Om prosjektet" />
+          </div>
+          <div className="mb-4" onClick={toggleSidebar}>
+            <Navlink to="/bidra" label="Bidra" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
